440dfd2919fefed570a57ed30014b021d874747b
jonnelafin <elias.eelinen@gmail.com>
2019-03-20 15:07:21 +0200

    done

--- test/test/src/test/Test.java
+++ test/test/src/test/Test.java
@@ -1,27 +1,21 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package viridiengine;
-
-import javax.swing.SwingUtilities;
+package test;
 
 /**
  *
  * @author Elias
  */
-public class main {
+public class Test {
 
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        SwingUtilities.invokeLater( new windowManager() );
     }
     
 }
