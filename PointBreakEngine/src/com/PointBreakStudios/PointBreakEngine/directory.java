4665bbaad8f6a275d06a05d355e798dba21f09cd
jonnelafin <elias.eelinen@gmail.com>
2019-06-17 22:38:57 +0300

    added directory class for easy and consistant directory access

---
+++ PointBreakEngine/src/com/PointBreakStudios/PointBreakEngine/directory.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.PointBreakStudios.PointBreakEngine;
+
+/**
+ *
+ * @author Jonnelafin
+ */
+public class directory {
+    public final static String levels = "/assets/levels/";
+    public final static String textures = "/assets/textures/";
+    public final static String replays = "/assets/replays/";
+    public final static String music = "/assets/music/";
+}
